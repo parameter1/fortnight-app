94f12028c7e9e1aba257e9965e76dbe68a8f5fab
Jacob Bare <jacob@parameter1.com>
2020-12-05 19:05:51 -0600

    Create email deployment edit placements list

---
+++ app/routes/manage/email-deployment/edit/email-placements.js
@@ -0,0 +1,45 @@
+import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+import { getObservable } from 'ember-apollo-client';
+
+import query from 'fortnight/gql/queries/email-deployment/edit/placements';
+
+export default Route.extend(RouteQueryManager, {
+  queryParams: {
+    first: {
+      refreshModel: true
+    },
+    sortBy: {
+      refreshModel: true
+    },
+    ascending: {
+      refreshModel: true
+    },
+  },
+
+  model({ first, sortBy, ascending }) {
+    const controller = this.controllerFor(this.get('routeName'));
+
+    const id = this.modelFor('manage.email-deployment.edit').get('id');
+    const input = { id };
+
+    const pagination = { first };
+    const sort = { field: sortBy, order: ascending ? 1 : -1 };
+
+    const variables = {
+      input,
+      pagination,
+      sort,
+    };
+    if (!sortBy) delete variables.sort.field;
+
+    const resultKey = 'emailDeployment';
+    controller.set('resultKey', resultKey);
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, resultKey)
+      .then((result) => {
+        controller.set('observable', getObservable(result));
+        return result;
+      }).catch(e => this.get('graphErrors').show(e))
+    ;
+  },
+});
