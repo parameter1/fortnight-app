d8cd32bd805487b585471f81999583740f0f92b1
Jacob Bare <jacob@parameter1.com>
2020-12-08 16:49:18 +0100

    Update line item date status to use mixin

--- app/components/email-line-item/date-status.js
+++ app/components/email-line-item/date-status.js
@@ -1,58 +1,20 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import moment from 'moment';
+import LineItemDatesMixin from 'fortnight/mixins/line-item-dates-mixin';
 
-const { isArray } = Array;
-
-export default Component.extend({
-  start: null,
-  end: null,
-  days: null,
-  type: null,
-
-  dateFormat: 'ddd MMM Do, YYYY',
-
-  showDateRange: computed('type', function() {
-    return this.get('type') === 'range';
+export default Component.extend(LineItemDatesMixin, {
+  joinedDays: computed('formattedDays.[]', function() {
+    return this.get('formattedDays').join(' | ');
   }),
 
-  daysFormatted: computed('_days.[]', function() {
-    return this.get('_days').map(day => day.format(this.get('dateFormat'))).join(' | ');
-  }),
-
-  rangeFormatted: computed('_range.{start,end}', function() {
-    const start = this.get('_range.start').format(this.get('dateFormat'));
-    const end = this.get('_range.end').format(this.get('dateFormat'));
+  joinedRange: computed('formattedRange.{start,end}', function() {
+    const start = this.get('formattedRange.start');
+    const end = this.get('formattedRange.end');
     return `${start} - ${end}`;
-  }),
-
-  _days: computed('days.[]', function() {
-    return this.get('daysSorted').map(d => moment(d));
-  }),
-
-  _range: computed('start', 'end', function() {
-    const start = this.get('start');
-    const end = this.get('end');
-    return {
-      start: start ? moment(start) : null,
-      end: end ? moment(end) : null,
-    }
-  }),
-
-  daysSorted: computed('_days.[]', function() {
-    return this.get('days').sort();
   }),
 
   label: computed('type', function() {
     if (this.get('type') === 'range') return 'Date Range:'
     return 'Days:';
   }),
-
-  init() {
-    this._super(...arguments);
-    const days = this.get('days');
-    this.set('days', isArray(days) ? days : []);
-    const type = this.get('type');
-    this.set('type', type || 'days');
-  },
 });
