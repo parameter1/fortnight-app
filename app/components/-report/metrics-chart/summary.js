92faf48934a1a1e8974137b0f7a3c151fe017100
Josh Worden <josh@parameter1.com>
2021-03-26 15:12:10 -0500

    Add summary display to metrics chart

---
+++ app/components/-report/metrics-chart/summary.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import InitValueMixin from 'fortnight/mixins/init-value';
+import { computed } from '@ember/object';
+
+export default Component.extend(InitValueMixin, {
+  /**
+   * The metric options, as an array of metric objects.
+   * For example:
+   * `[ { key: 'views', label: 'Impressions' } ]`
+   *
+   * @type {object[]}
+   */
+  options: null,
+
+  /**
+   * The raw metric data
+   *
+   * @type {object[]}
+   */
+  rows: null,
+
+  init() {
+    this._super(...arguments);
+    this.initValue('options', []);
+    this.initValue('rows', []);
+  },
+
+  metrics: computed('options', 'rows', function() {
+    return this.options.map((opt) => ({
+      label: opt.label,
+      total: this.rows.reduce((sum, { metrics }) => sum += metrics[opt.key] || 0, 0),
+    }));
+  }),
+});
